f7223618fe381d10e67da3ed93e650db695b36c5
mojtaba <mojtaba@MacAroni.local>
2023-05-27 13:21:50 +0330

    finalV

--- src/components/oders.jsx
+++ src/components/oders.jsx
@@ -2,27 +2,51 @@ import * as React from 'react';
 // import BasicPopover from './popover';
 
 function OrderShower(props){
-    const [card, setCard] = React.useState(true)
+    const [card, setCard] = React.useState(false)
     function flipper (){
         setCard(card => !card)
     }
     return(
-
+<>
             <div>
-                <div className={card ? "order" : "order-magnified"} typeof='button' onClick={flipper}>
-                    <img className={card ? "order--pic" : "order--pic-magnified"} src={props.img} alt="order" />
-                    <div className={card ? "order--info" : "order--info-magnified"}>
-                        <div className={card ? "order--info" : "order--info-magnified"}>
-                            <span className={card ? "order--name" : "order--name-magnified"}>{props.name}</span>
-                            <span className={card ? "order--price" : "order--price-magnified"}>{props.price}</span>
-                            <div className='order--ingridients display-linebreak'>{card ? "" :`: توضیحات‌ ${props.description}`}</div>
+                <div className="order" typeof='button' onClick={flipper}>
+                    <img className="order--pic" src={props.img} alt="order" />
+                    <div className="order--info">
+                        <div className="order--info" >
+                            <span className="order--name" >{props.name}</span>
+                            <span className="order--price" >{props.price}</span>
                         </div>
-                        
                     </div>
-                    
                 </div>  
             </div>
-       
+            {card && (
+                    <div className='backdrop' onClick={()=>{
+                        setCard(card => !card)
+                    }}>
+                        <div className='product-modal'>
+                            {/* <div className='modal-content'> */}
+                                {/* <div className="order--modal" typeof='button' onClick={flipper}>
+                                    <img className="order--pic--modal" src={props.img} alt="order" />
+                                    <div className='info--modal'>
+                                    <p className="order--name--modal" >{props.name}</p>
+                                    <p className="order--price--modal" >{props.price}</p>
+                                    </div>
+                                </div>   */}
+                                <div className="order" typeof='button' onClick={flipper}>
+                                    <img className="order--pic--modal" src={props.img} alt="order" />
+                                    <div className="order--info">
+                                        <div className="order--info" >
+                                            <span className="order--name" >{props.name}</span>
+                                            <span className="order--price" >{props.price}</span>
+                                            <span className='order--description' >{props.description}</span>
+                                        </div>
+                                    </div>
+                                </div>  
+                            </div>
+                        {/* </div> */}
+                    </div>
+                )}
+</>
     )
 }
 export default OrderShower;
