62fbff5c6f0ba2300987e0dc5b306d2e309ad27e
mojtaba <mojtaba@MacAroni.local>
2023-07-21 12:43:23 +0330

    with loading animation

--- src/components/oders.jsx
+++ src/components/oders.jsx
@@ -1,52 +1,53 @@
-import * as React from 'react';
-// import BasicPopover from './popover';
+import React, { useState, useEffect } from 'react';
 
-function OrderShower(props){
-    const [card, setCard] = React.useState(false)
-    function flipper (){
-        setCard(card => !card)
-    }
-    return(
-<>
-            <div>
+function OrderShower(props) {
+  const [card, setCard] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a 4-second loading time
+    const timer = setTimeout(() => setLoading(false), 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  function flipper() {
+    setCard((prevCard) => !prevCard);
+  }
+
+  return (
+    <>
+      <div>
+        <div className="order" typeof="button" onClick={flipper}>
+          {loading ? (
+            <div className="loading-placeholder" />
+          ) : (
+            <img className="order--pic" src={props.img} alt="order" />
+          )}
+          <div className="order--info">
+            <div className="order--info">
+              <span className="order--name">{props.name}</span>
+              <span className="order--price">{props.price}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {card && (
+        <div className="backdrop" onClick={() => setCard((prevCard) => !prevCard)}>
+            <div className="product-modal">
                 <div className="order" typeof='button' onClick={flipper}>
-                    <img className="order--pic" src={props.img} alt="order" />
-                    <div className="order--info">
+                    <img className="order--pic--modal" src={props.img} alt="order" />
                         <div className="order--info" >
                             <span className="order--name" >{props.name}</span>
                             <span className="order--price" >{props.price}</span>
+                            <span className='order--description' >{props.description}</span>
                         </div>
-                    </div>
-                </div>  
+                </div>           
             </div>
-            {card && (
-                    <div className='backdrop' onClick={()=>{
-                        setCard(card => !card)
-                    }}>
-                        <div className='product-modal'>
-                            {/* <div className='modal-content'> */}
-                                {/* <div className="order--modal" typeof='button' onClick={flipper}>
-                                    <img className="order--pic--modal" src={props.img} alt="order" />
-                                    <div className='info--modal'>
-                                    <p className="order--name--modal" >{props.name}</p>
-                                    <p className="order--price--modal" >{props.price}</p>
-                                    </div>
-                                </div>   */}
-                                <div className="order" typeof='button' onClick={flipper}>
-                                    <img className="order--pic--modal" src={props.img} alt="order" />
-                                    <div className="order--info">
-                                        <div className="order--info" >
-                                            <span className="order--name" >{props.name}</span>
-                                            <span className="order--price" >{props.price}</span>
-                                            <span className='order--description' >{props.description}</span>
-                                        </div>
-                                    </div>
-                                </div>  
-                            </div>
-                        {/* </div> */}
-                    </div>
-                )}
-</>
-    )
+        </div>
+      )}
+    </>
+  );
 }
+
 export default OrderShower;
